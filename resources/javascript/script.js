1f943d828385ead1bfcef44cccde7850a87d404c
inevitable-hayashi <hayashiflex@gmail.com>
2025-03-12 15:53:44 +0900

    Uncaught ReferenceError: Spine is not defined画出る理由が分からん

---
+++ resources/javascript/script.js
@@ -0,0 +1,19 @@
+
+const app = new PIXI.Application();
+await app.init({ width: 640, height: 600, backgroundAlpha: 0 });
+// pixiアプリケーションを作る
+
+
+// コンテナに格納
+const el = document.getElementById("yuunicorn_screen");
+el.appendChild(app.canvas);
+
+// yuunicornを読み込む
+PIXI.Assets.add({ alias: "skeleton-data", src: "../spine/yuunicorn/IMG_4693.json" });
+PIXI.Assets.add({ alias: "skeleton-atlas", src: "../spine/yuunicorn/IMG_4693.atlas" });
+await PIXI.Assets.load["skeleton-data", "skeleton-atlas"];
+// assetsは大文字
+    
+const yuunicorn = Spine.from({skelton: "IMG_4693", atlas: "IMG_4693"});
+
+app.stage.addChild(yuunicorn);
